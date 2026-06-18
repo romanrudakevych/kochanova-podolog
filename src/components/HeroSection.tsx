7200b25f5c51877aa8b3a42a349ab6641f3557f3
romanrudakevych <roood90@gmail.com>
2026-06-18 12:00:06 +0200

    replace hero image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Phone, ArrowDown } from "lucide-react";
 import { useTranslation } from "react-i18next";
-import heroImg from "@/assets/hero.jpg";
+import heroImg from "@/assets/hero.webp";
 
 const HeroSection = () => {
   const { t } = useTranslation();
